5e73ec7e987ef053046ad25b2b73d58d218fa9a2
Guenael Labois <glabois@h4o-studio.com>
2019-09-25 09:03:39 -0500

    fix-bug

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,27 +4,28 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 const mkdir = require('mkdirp');
 
-const { lstatSync, readdirSync } = require('fs')
-const { join, basename } = require('path')
-
-
+const { lstatSync, readdirSync } = require('fs');
+const { join, basename } = require('path');
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Bootstraps docker environments for many platforms'
-    ));
-    const isDirectory = source => lstatSync(source).isDirectory()
-    const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory)
-    const friendlyName = source => source.map(name => basename(name) )
+    this.log(yosay('Bootstraps docker environments for many platforms'));
+    const isDirectory = source => lstatSync(source).isDirectory();
+    const getDirectories = source =>
+      readdirSync(source)
+        .map(name => join(source, name))
+        .filter(isDirectory);
+    const friendlyName = source => source.map(name => basename(name));
 
-    const prompts = [{
-      type: 'list',
-      name: 'stack',
-      choices: friendlyName(getDirectories(this.sourceRoot())),
-      message: 'Project template:'
-    }];
+    const prompts = [
+      {
+        type: 'list',
+        name: 'stack',
+        choices: friendlyName(getDirectories(this.sourceRoot())),
+        message: 'Project template:'
+      }
+    ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
@@ -33,12 +34,12 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    var Processor = require(join(this.sourceRoot(), this.props.stack, 'index.js'))
-    var writer = new Processor()
-    writer.process(this)
+    var Processor = require(join(this.sourceRoot(), this.props.stack, 'index.js'));
+    var writer = new Processor();
+    writer.process(this);
   }
 
   install() {
-   // this.installDependencies();
+    // This.installDependencies();
   }
 };
